8f7549824676cda259701b3e645796fb0954686b
Farzat <Farzat.UG@outlook.jp>
2019-10-23 07:36:03 +0900

    Can now create app installer through pynsist.

--- tabs.py
+++ tabs.py
@@ -55,60 +55,62 @@ def quitProgram(root):
     sys.exit()
 
 
-root = tk.Tk()
-root.title(" Twitter Data Analysis")
-root.geometry("1000x700")
+def main():
+    root = tk.Tk()
+    root.title(" Twitter Data Analysis")
+    root.geometry("1000x700")
 
-# ***** Menu bar ***********
-menu = tk.Menu(root)
-root.config(menu=menu)
+    # ***** Menu bar ***********
+    menu = tk.Menu(root)
+    root.config(menu=menu)
 
-subMenu = tk.Menu(menu, tearoff=0)
-menu.add_cascade(label="File", menu=subMenu)
-subMenu.add_command(label="New Analysis...", command= lambda: gc.AnalysisTab(notebook, woeid_data))
-subMenu.add_command(label="Save Analysis as...", command = doNothing)
-subMenu.add_separator()
-subMenu.add_command(label="Exit", command = lambda: quitProgram(root))
+    subMenu = tk.Menu(menu, tearoff=0)
+    menu.add_cascade(label="File", menu=subMenu)
+    subMenu.add_command(label="New Analysis...", command= lambda: gc.AnalysisTab(notebook, woeid_data))
+    subMenu.add_command(label="Save Analysis as...", command = doNothing)
+    subMenu.add_separator()
+    subMenu.add_command(label="Exit", command = lambda: quitProgram(root))
 
-editMenu = tk.Menu(menu, tearoff=0)
-menu.add_cascade(label="Edit", menu=editMenu)
-editMenu.add_command(label="Advanced...", command = lambda: gc.AdvancedTab(notebook, data))
+    editMenu = tk.Menu(menu, tearoff=0)
+    menu.add_cascade(label="Edit", menu=editMenu)
+    editMenu.add_command(label="Advanced...", command = lambda: gc.AdvancedTab(notebook, data))
 
-# ***** Tool bar ***********
+    # ***** Tool bar ***********
 
-toolbar = tk.Frame(root, bg="grey")
+    toolbar = tk.Frame(root, bg="grey")
 
-option1Button = tk.Button(toolbar, text="Option 1", command=doNothing)
-option1Button.pack(side="left", padx=2, pady=2)
-option2Button = tk.Button(toolbar, text="Option 2", command=doNothing)
-option2Button.pack(side="left", padx=2, pady=2)
+    option1Button = tk.Button(toolbar, text="Option 1", command=doNothing)
+    option1Button.pack(side="left", padx=2, pady=2)
+    option2Button = tk.Button(toolbar, text="Option 2", command=doNothing)
+    option2Button.pack(side="left", padx=2, pady=2)
 
-toolbar.pack(side="top", fill = "x")
+    toolbar.pack(side="top", fill = "x")
 
-# ***** Status bar ******
-statusbar = tk.Label(root, text="Disconnected from server...", bd=1, relief="sunken", anchor="w")
-statusbar.pack(side="bottom", fill="x")
+    # ***** Status bar ******
+    statusbar = tk.Label(root, text="Disconnected from server...", bd=1, relief="sunken", anchor="w")
+    statusbar.pack(side="bottom", fill="x")
 
-notebook = gc.CustomNotebook(root,  close_button = True)
-notebook.pack(side="top", fill="both", expand=True)
-"""
-test_tab = gc.Tab(notebook, "Test")
-test_tab2 = gc.Tab(notebook, "Test2")
-
-
-labelFrame = tk.LabelFrame(test_tab.frame, text ="Testing stuff")
-some_frame = tk.Frame(labelFrame, width = 400, height = 400)
-some_frame.pack()
-labelFrame.pack(anchor= "nw")
-
-test_notebook = gc.FramedNotebook(some_frame, text = "FUCKSIES!", close_button = True)
-test_notebook.pack()
-
-test_button = tk.Button(some_frame, text = "Boom!", command = lambda: print(notebook.tabs[notebook.tabs.index(test_tab2)].tabName))
-test_button.place(relx = 0.5, rely=0.5, anchor = tk.CENTER)
-"""
+    notebook = gc.CustomNotebook(root,  close_button = True)
+    notebook.pack(side="top", fill="both", expand=True)
+    """
+    test_tab = gc.Tab(notebook, "Test")
+    test_tab2 = gc.Tab(notebook, "Test2")
 
 
-root.mainloop()
+    labelFrame = tk.LabelFrame(test_tab.frame, text ="Testing stuff")
+    some_frame = tk.Frame(labelFrame, width = 400, height = 400)
+    some_frame.pack()
+    labelFrame.pack(anchor= "nw")
+
+    test_notebook = gc.FramedNotebook(some_frame, text = "FUCKSIES!", close_button = True)
+    test_notebook.pack()
+
+    test_button = tk.Button(some_frame, text = "Boom!", command = lambda: print(notebook.tabs[notebook.tabs.index(test_tab2)].tabName))
+    test_button.place(relx = 0.5, rely=0.5, anchor = tk.CENTER)
+    """
 
 
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
